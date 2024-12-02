eb22ab925335b3c060c96895c13fb295f058714f
Snipy7374 <100313469+Snipy7374@users.noreply.github.com>
2024-12-02 18:58:56 +0100

    day 2 partial

---
+++ aoc_2024/day_2_2.py
@@ -0,0 +1,1 @@
+# I feel like this problem can be solved using dynamic programming
---
+++ scripts/script_day_2_1.py
@@ -0,0 +1,14 @@
+from scripts.constants import ASSETS_PATH
+from aoc_2024.day_2_1 import analyze_safe_reports
+
+
+if __name__ == "__main__":
+    reports: list[list[int]] = []
+    with open(ASSETS_PATH / "day_2.txt") as f:
+        data = f.readlines()
+    
+    for line in data:
+        reports.append(list(map(int, line.split(" "))))
+    
+    solution = analyze_safe_reports(reports)
+    print(f"The total number of safe reports is: {solution}")
