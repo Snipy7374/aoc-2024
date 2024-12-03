bc00c5fe770a6ed6bb3705a6df57d3123bb0a432
Snipy7374 <100313469+Snipy7374@users.noreply.github.com>
2024-12-03 16:28:11 +0100

    remove what's not my solution

--- aoc_2024/day_3.py
+++ aoc_2024/day_3.py
@@ -9,21 +9,3 @@ def mul(instructions: str) -> int:
     for (a, b) in pairs:
         result += int(a) * int(b)
     return result
-
-
-# not my solution (wasn't able to come up with a regex simple enough)
-# made by https://github.com/Starwort
-CONDITIONAL_MUL_PATTERN = r"mul\((\d+),(\d+)\)|(do\(\))|(don't\(\))"
-def mul_pt_2(instructions: str) -> int:
-    data = re.findall(CONDITIONAL_MUL_PATTERN, instructions)
-    on = True
-    result = 0
-    for a, b, do, dont in data:
-        if dont:
-            on = False
-        elif do:
-            on = True
-        elif on:
-            result += int(a) * int(b)
-
-    return result
